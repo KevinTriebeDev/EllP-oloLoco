94cba1454adf08c65377d4261b9f8b4987ebef5e
KeinTriebeDev <gamingtriebe@gmail.com>
2026-05-27 21:08:39 +0200

    char pepe added animation

--- js/models/caracter.class.js
+++ js/models/caracter.class.js
@@ -1,11 +1,33 @@
 class caracter extends MovableObject {
-         width = 100;
-        height = 200;
+    width = 100;
+    height = 200;
+    IMAGES_WALKING = [
+                "assets/img/2_character_pepe/2_walk/W-21.png",
+                "assets/img/2_character_pepe/2_walk/W-22.png",
+                "assets/img/2_character_pepe/2_walk/W-23.png",
+                "assets/img/2_character_pepe/2_walk/W-24.png",
+                "assets/img/2_character_pepe/2_walk/W-25.png",
+                "assets/img/2_character_pepe/2_walk/W-26.png",
+            ];
+            currentImage = 0;
         constructor() {
             super();
-            this.loadImg("assets/img/2_character_pepe/1_idle/idle/I-1.png");
+            this.loadImg("assets/img/2_character_pepe/2_walk/W-21.png");
             this.x = 120;
             this.y = 230;
+            this.loadImages(this.IMAGES_WALKING);
+            this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+        let i = this.currentImage % this.IMAGES_WALKING.length;
+        let path = this.IMAGES_WALKING[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+
+    }, 100);
+
     }
     jump() {
 
--- js/models/movable-object.class.js
+++ js/models/movable-object.class.js
@@ -4,10 +4,18 @@ class MovableObject {
     img;
     height = 150;
     width = 100;
-
+    imageCache = [];
     loadImg(path) {
         this.img = new Image();
         this.img.src = path;
+    }
+
+    loadImages(arr) {
+        arr.forEach((path) => {       
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+    });
     }
 
     moveRight() {
